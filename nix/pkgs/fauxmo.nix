afc25aa2c9643e2ba888093ef1a8f7c0541d5a83
Henry Estela <henryestela@gmail.com>
2018-04-18 18:10:49 -0700

    Use fork of fauxmo which adds raw POST data support

--- nix/pkgs/fauxmo.nix
+++ nix/pkgs/fauxmo.nix
@@ -9,12 +9,13 @@ python3Packages.buildPythonApplication rec {
   ];
 
   name = "fauxmo-${version}";
-  version = "0.4.6";
+  version = "0.4.7";
 
   doCheck = false;
 
-  src = fetchurl {
-    url = "mirror://pypi/f/fauxmo/fauxmo-${version}.tar.gz";
-    sha256 = "1j0h7rrj1r2vvv74s1ldqc9ipx7qbvmknkmxlcpsy35pklkb7a1k";
+  src = fetchgit {
+    url = "https://github.com/hestela/fauxmo";
+    rev = "f2246b952a0246bf0dcd74e770c9daea0dcaa763";
+    sha256 = "0mfmbs09irh39ldak55lzsai9j3wmf0zwi0javnna6xkb2j1yh2i";
   };
 }
